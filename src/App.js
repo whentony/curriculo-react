d38e2957af179a3340a8433d998fa8949d9a089e
whentony <whentony.ferreira@gmail.com>
2021-02-18 20:36:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  } from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+       <Text>Currículo</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
